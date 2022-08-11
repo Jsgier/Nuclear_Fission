5c484edf5da605311d12708ad95dbab458b70659
Josh Gier <josh@soeffects.com>
2022-08-10 21:46:45 -0700

    added some functions

--- Nuclear_Fission_Scratchpad.py
+++ Nuclear_Fission_Scratchpad.py
@@ -3,6 +3,27 @@
 
 import numpy
 
+# Constants
+N_A = 6.022E23 #Avogadro's number, mol^-1
+
+# User Defined Functions
+
+#dP- probability of reaction
+#N - number of nuclei per unit volume
+#sigma - cross section. Note - total cross section is sum of all cross sections
+#dx - short distance traveled by neutron
+dP = lambda N,sigma,dx : N*sigma*dx
+
+#N- number of nuclei per unit volume
+#rho - density
+#N_A - Avogadro's number
+#M - material atomic mass
+N = lambda rho,M : N_A*rho / M
+
+#mfp - mean free path
+mfp = lambda N, sigma: 1 / (N*sigma)
+
+
 def main():
 
 

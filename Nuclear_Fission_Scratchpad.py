9824f63051a306969b1de0303a63b0dd82bd9443
Josh Gier <josh@soeffects.com>
2022-08-10 23:10:35 -0700

    EOD commit

--- Nuclear_Fission_Scratchpad.py
+++ Nuclear_Fission_Scratchpad.py
@@ -1,5 +1,6 @@
 
-#purpose of this script is to provide a scratchpad for
+#purpose of this script is to provide a scratchpad for nuclear fission research
+#source: Nuclear Energy: Principles, Practices and Prospects
 
 import numpy
 
@@ -8,6 +9,7 @@ N_A = 6.022E23 #Avogadro's number, mol^-1
 
 # User Defined Functions
 
+#eq. 5.1
 #dP- probability of reaction
 #N - number of nuclei per unit volume
 #sigma - cross section. Note - total cross section is sum of all cross sections
@@ -20,9 +22,11 @@ dP = lambda N,sigma,dx : N*sigma*dx
 #M - material atomic mass
 N = lambda rho,M : N_A*rho / M
 
+#eq. 5.3
 #mfp - mean free path
 mfp = lambda N, sigma: 1 / (N*sigma)
 
+#eq. 5.5
 #Breit-Wigner formula
 #sigma_a - absorption cross section, which approximates cross section of
 #compound nucleus
